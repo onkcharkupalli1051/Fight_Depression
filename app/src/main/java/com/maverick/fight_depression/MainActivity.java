37ed538fb6a21197e77b4bf9cf4e7bc002dfd854
Onkar Charkupalli <onkarcharkupalli@gmail.com>
2022-11-12 13:59:33 +0530

    Gradle update

--- app/src/main/java/com/maverick/fight_depression/MainActivity.java
+++ app/src/main/java/com/maverick/fight_depression/MainActivity.java
@@ -6,12 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private Button registerButton;
-    private TextView loginButton;
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +17,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Fight Depression");
-        registerButton = findViewById(R.id.idActivityMainBtnRegister);
-        loginButton = findViewById(R.id.idActivityMainLoginBtn);
+        startButton = findViewById(R.id.idActivityMainBtnStart);
         
-        registerButton.setOnClickListener(new View.OnClickListener() {
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+//            }
+//        });
+        
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this, "Login Intent", Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
-            }
-        });
-        
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Login Intent", Toast.LENGTH_SHORT).show();
                 startActivity(new Intent(MainActivity.this, enquireActivity.class));
             }
         });
